8f5a5621b873ea18548755606cc173a4427a2ed4
Tianer Zhou <ezhoureal@gmail.com>
2025-05-11 17:05:16 +0800

    benchmark script
    Signed-off-by: Tianer Zhou <ezhoureal@gmail.com>

---
+++ cs336_systems/benchmark.py
@@ -0,0 +1,54 @@
+import time
+from cs336_basics import model
+import torch
+class ModelSize:
+    d_model: int
+    d_ff: int
+    num_layers: int
+    num_heads: int
+    def __init__(self, d_model, d_ff, num_layers, num_heads):
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+
+SMALL = ModelSize(d_model=768, d_ff=3072, num_layers=12, num_heads=12)
+MEDIUM = ModelSize(d_model=1024, d_ff=4096, num_layers=24, num_heads=16)
+LARGE = ModelSize(d_model=1280, d_ff=5120, num_layers=36, num_heads=20)
+XL = ModelSize(d_model=1600, d_ff=6400, num_layers=48, num_heads=25)
+XXL = ModelSize(d_model=2560, d_ff=10240, num_layers=32, num_heads=32)
+
+W = 0
+CONTEXT_LENGTH = 512
+ROPE_THETA = 10000
+VOCAB_SIZE = 30512
+B = 32
+
+device = (
+    torch.device("cuda") if torch.cuda.is_available() else
+    torch.device("cpu")
+)
+
+def benchmark(size: ModelSize, forward_only: bool, steps: int):
+    m = model.BasicsTransformerLM(vocab_size=VOCAB_SIZE, context_length=CONTEXT_LENGTH, d_model=size.d_model, num_layers=size.num_layers, num_heads=size.num_heads, d_ff=size.d_ff, rope_theta=ROPE_THETA)
+    random_batch = torch.randint(0, VOCAB_SIZE, (B, CONTEXT_LENGTH), dtype=torch.int32)
+    stats: list[int] = []
+    for i in range(W + steps):
+        # warmup
+        start_time = time.time()
+        pred = m(random_batch)
+        if not forward_only:
+            loss = torch.mean(pred)
+            loss.backward()
+        if device == torch.device('cuda'):
+            torch.cuda.synchronize()
+        end_time = time.time()
+        if i >= W:
+            # record
+            stats.append(end_time - start_time)
+    print(stats)
+    avg = sum(stats) / len(stats)
+    std = (sum((x - avg) ** 2 for x in stats) / len(stats)) ** 0.5
+    print(f'avg time = {avg:.3f} seconds, std = {std:.3f}')
+
+benchmark(SMALL, True, 1)
